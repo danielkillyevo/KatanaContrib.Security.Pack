235f4359e3ef9c93d373d09bf1dcdd347b9e87cd
lpjanaka <lpjanaka@gmail.com>
2014-02-11 23:35:32 +0530

    LinkedInAuthenticationExtensionsTests UnitTest01

---
+++ KatanaContrib.Security.LinkedIn.Tests/LinkedInAuthenticationExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KatanaContrib.Security.LinkedIn;
+using Owin;
+
+namespace KatanaContrib.Security.LinkedIn.Tests
+{
+    [TestClass]
+    public class LinkedInAuthenticationExtensionsTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void UseLinkedInAuthentication_WhenAppArgumentIsNull_ShouldThrowArgumentNull()
+        {
+            var options = new LinkedInAuthenticationOptions();
+            IAppBuilder app = null;
+
+            LinkedInAuthenticationExtensions.UseLinkedInAuthentication(app, options);
+        }
+
+    }
+}
